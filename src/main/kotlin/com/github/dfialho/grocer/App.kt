b287558392c66902de3da51d4f3c78c382928468
David Fialho <dfialho@protonmail.com>
2022-09-03 23:50:27 +0100

    Take configurations using CDI

--- src/main/kotlin/com/github/dfialho/grocer/App.kt
+++ src/main/kotlin/com/github/dfialho/grocer/App.kt
@@ -4,17 +4,17 @@ import com.github.dfialho.grocer.continenteonline.ContinenteOnlineGrocer
 import io.agroal.api.AgroalDataSource
 import io.quarkus.runtime.ShutdownEvent
 import io.quarkus.runtime.StartupEvent
-import org.eclipse.microprofile.config.ConfigProvider
+import org.eclipse.microprofile.config.inject.ConfigProperty
 import java.nio.file.Paths
 import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.event.Observes
 
 @ApplicationScoped
-class App(dataSource: AgroalDataSource) {
-
-    private val watchDirectory: String = ConfigProvider.getConfig().getValue("grocer.continenteonline.directory", String::class.java)
-    private val rulesFile: String = ConfigProvider.getConfig().getValue("grocer.continenteonline.rulesFile", String::class.java)
-
+class App(
+    @Suppress("CdiInjectionPointsInspection") dataSource: AgroalDataSource,
+    @ConfigProperty(name = "grocer.continenteonline.directory") watchDirectory: String,
+    @ConfigProperty(name = "grocer.continenteonline.rulesFile") rulesFile: String
+) {
     private val grocer = ContinenteOnlineGrocer(
         watchDirectory = Paths.get(watchDirectory),
         rulesFile = Paths.get(rulesFile),
